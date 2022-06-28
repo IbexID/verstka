67c78dd73c7bc626aae2aacc8b908dd2e7e2f452
Ibex_id <romquee@gmail.com>
2022-06-28 08:11:37 +0300

    реализовано выпадающее меню в мобильной версии

--- src/legalide.js
+++ src/legalide.js
@@ -1,2 +1,19 @@
 
 import './legalide.scss'
+const burgerMenu = document.querySelector('.header__nav-burger')
+const nav = document.querySelector('.header__nav')
+const menu = document.querySelector('ul.header__nav-list')
+const width = document.body.clientWidth
+
+if(width < 867 && burgerMenu){
+    burgerMenu.addEventListener('click', (()=>{
+        console.log(menu.style.display)
+        if(!menu.classList.contains('hidden-menu')){
+        menu.classList.toggle('hidden-menu');
+        nav.style.marginBottom = '0'
+        } else {
+            menu.classList.toggle('hidden-menu');
+        nav.style.marginBottom = '460px'
+        }
+        }))
+}
